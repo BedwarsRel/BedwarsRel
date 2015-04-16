ad18773dc9dafdf1780bae57384a3f83735529c9
Yannici <yannicschne@gmail.com>
2015-04-17 00:56:38 +0200

    Small unload fix

--- src/io/github/yannici/bedwars/Game/GameManager.java
+++ src/io/github/yannici/bedwars/Game/GameManager.java
@@ -187,6 +187,7 @@ public class GameManager {
                 g.stop();
             }
             
+            g.setState(GameState.STOPPED);
             g.setScoreboard(Main.getInstance().getScoreboardManager().getNewScoreboard());
             g.kickAllPlayers();
             g.resetRegion();
