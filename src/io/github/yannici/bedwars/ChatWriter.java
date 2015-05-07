7274ecdac7e141c3b234c0dd7268431e409fbe0b
Yannici <yannicschne@gmail.com>
2015-05-07 09:04:59 +0200

    [1.0.79] Added config for system message chat prefix (closes #119)

--- src/io/github/yannici/bedwars/ChatWriter.java
+++ src/io/github/yannici/bedwars/ChatWriter.java
@@ -9,7 +9,7 @@ public class ChatWriter {
 	}
 
 	public static String pluginMessage(String str) {
-		return ChatColor.GOLD + "[Bedwars] "
+		return Main.getInstance().getConfig().getString("chat-prefix", ChatColor.GRAY + "[" + ChatColor.AQUA + "BedWars" + ChatColor.GRAY + "]") + " "
 				+ ChatColor.WHITE + str;
 	}
 
