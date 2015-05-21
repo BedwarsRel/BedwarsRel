7d655ce876966a64cf95dbae5119c96258c59b5b
Yannici <yannicschne@gmail.com>
2015-05-21 12:04:10 +0200

    [1.1.10] Trap compatibility for >= 1.7.9 fix

--- src/io/github/yannici/bedwars/Shop/Specials/Trap.java
+++ src/io/github/yannici/bedwars/Shop/Specials/Trap.java
@@ -1,5 +1,6 @@
 package io.github.yannici.bedwars.Shop.Specials;
 
+import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -52,57 +53,97 @@ public class Trap extends SpecialItem {
 		return null;
 	}
 	
+	private Constructor<PotionEffect> getPotionConstructor() {
+		Constructor<PotionEffect> effectConstructor = null;
+		try {
+			effectConstructor = PotionEffect.class.getConstructor(PotionEffectType.class, int.class, int.class, boolean.class, boolean.class);
+			return effectConstructor;
+		} catch(Exception ex) {
+			// no constr
+		}
+		
+		try {
+			effectConstructor = PotionEffect.class.getConstructor(PotionEffectType.class, int.class, int.class, boolean.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return effectConstructor;
+	}
+	
 	public void activate(final Player player) {
-		List<PotionEffect> effects = new ArrayList<PotionEffect>();
-		
-		if(this.activateBlindness) {
-			PotionEffect blind = new PotionEffect(PotionEffectType.BLINDNESS, this.duration*20, this.amplifierBlindness, true, this.particles);
-			effects.add(blind);
+		try {
+			List<PotionEffect> effects = new ArrayList<PotionEffect>();
+			Constructor<PotionEffect> potionEffectConstr = this.getPotionConstructor();
+			
+			if(this.activateBlindness) {
+				PotionEffect blind = null;
+				
+				if(potionEffectConstr.getParameterTypes().length == 5) {
+					blind = potionEffectConstr.newInstance(PotionEffectType.BLINDNESS, this.duration*20, this.amplifierBlindness, true, this.particles);
+				} else {
+					blind = potionEffectConstr.newInstance(PotionEffectType.BLINDNESS, this.duration*20, this.amplifierBlindness, true);
+				}
+				
+				effects.add(blind);
+			}
+			
+			if(this.activateWeakness) {
+				PotionEffect weak = null;
+				if(potionEffectConstr.getParameterTypes().length == 5) {
+					weak = potionEffectConstr.newInstance(PotionEffectType.WEAKNESS, this.duration*20, this.amplifierWeakness, true, this.particles);
+				} else {
+					weak = potionEffectConstr.newInstance(PotionEffectType.WEAKNESS, this.duration*20, this.amplifierWeakness, true);
+				}
+				effects.add(weak);
+			}
+			
+			if(this.activateSlowness) {
+				PotionEffect slow = null;
+				if(potionEffectConstr.getParameterTypes().length == 5) {
+					slow = potionEffectConstr.newInstance(PotionEffectType.SLOW, this.duration*20, this.amplifierSlowness, true, this.particles);
+				} else {
+					slow = potionEffectConstr.newInstance(PotionEffectType.SLOW, this.duration*20, this.amplifierSlowness, true);
+				}
+				
+				effects.add(slow);
+			}
+			
+			this.game.addRunningTask(new BukkitRunnable() {
+	            
+	            private int counter = 0;
+	            
+	            @Override
+	            public void run() {
+	                if(this.counter >= Trap.this.duration) {
+	                    Trap.this.game.removeRunningTask(this);
+	                    this.cancel();
+	                    return;
+	                }
+	                
+	                player.playSound(player.getLocation(), Sound.FUSE, 2.0F, 1.0F);
+	                this.counter++;
+	            }
+	        }.runTaskTimer(Main.getInstance(), 0L, 20L));
+			
+			if(effects.size() > 0) {
+			    for(PotionEffect effect : effects) {
+			        if(player.hasPotionEffect(effect.getType())) {
+			            player.removePotionEffect(effect.getType());
+			        }
+			        
+			        player.addPotionEffect(effect);
+			    }
+			}
+			
+			this.game.broadcast(Main._l("ingame.specials.trap.trapped"), new ArrayList<Player>(this.team.getPlayers()));
+			if(this.playSound) {
+				this.game.broadcastSound(Sound.SHEEP_IDLE, 3.0F, 1.0F, this.team.getPlayers());
+			}
+			this.getLocation().getBlock().setType(Material.AIR);
+			this.game.removeSpecialItem(this);
+		} catch(Exception ex) {
+			ex.printStackTrace();
 		}
-		
-		if(this.activateWeakness) {
-			PotionEffect weak = new PotionEffect(PotionEffectType.WEAKNESS, this.duration*20, this.amplifierWeakness, true, this.particles);
-			effects.add(weak);
-		}
-		
-		if(this.activateSlowness) {
-			PotionEffect slow = new PotionEffect(PotionEffectType.SLOW, this.duration*20, this.amplifierSlowness, true, this.particles);
-			effects.add(slow);
-		}
-		
-		this.game.addRunningTask(new BukkitRunnable() {
-            
-            private int counter = 0;
-            
-            @Override
-            public void run() {
-                if(this.counter >= Trap.this.duration) {
-                    Trap.this.game.removeRunningTask(this);
-                    this.cancel();
-                    return;
-                }
-                
-                player.playSound(player.getLocation(), Sound.FUSE, 2.0F, 1.0F);
-                this.counter++;
-            }
-        }.runTaskTimer(Main.getInstance(), 0L, 20L));
-		
-		if(effects.size() > 0) {
-		    for(PotionEffect effect : effects) {
-		        if(player.hasPotionEffect(effect.getType())) {
-		            player.removePotionEffect(effect.getType());
-		        }
-		        
-		        player.addPotionEffect(effect);
-		    }
-		}
-		
-		this.game.broadcast(Main._l("ingame.specials.trap.trapped"), new ArrayList<Player>(this.team.getPlayers()));
-		if(this.playSound) {
-			this.game.broadcastSound(Sound.SHEEP_IDLE, 3.0F, 1.0F, this.team.getPlayers());
-		}
-		this.getLocation().getBlock().setType(Material.AIR);
-		this.game.removeSpecialItem(this);
 	}
 	
 	public void create(Game game, Team team, Location location) {
