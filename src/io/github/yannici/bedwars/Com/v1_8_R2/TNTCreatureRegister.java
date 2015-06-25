27eb60c010541f7569d9c41bdb0c26a04cf8193c
Yannici <yannicschne@gmail.com>
2015-06-25 16:57:18 +0200

    [1.2.1] Removed uselesse bukkit runnable

--- src/io/github/yannici/bedwars/Com/v1_8_R2/TNTCreatureRegister.java
+++ src/io/github/yannici/bedwars/Com/v1_8_R2/TNTCreatureRegister.java
@@ -16,9 +16,7 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.TNTPrimed;
 import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
-import org.bukkit.scheduler.BukkitRunnable;
 
-import io.github.yannici.bedwars.Main;
 import io.github.yannici.bedwars.Shop.Specials.ITNTCreature;
 import io.github.yannici.bedwars.Shop.Specials.ITNTCreatureRegister;
 
@@ -64,28 +62,22 @@ public class TNTCreatureRegister implements ITNTCreatureRegister {
 	public ITNTCreature spawnCreature(final Location location, Player owner, Player target, final DyeColor color) {
 		final TNTCreature sheep = new TNTCreature(location.getWorld(), target);
 		
-		new BukkitRunnable() {
-			
-			@Override
-			public void run() {
-				((World) location.getWorld()).addEntity(sheep, SpawnReason.CUSTOM);
-				TNTPrimed primedTnt = (TNTPrimed) location.getWorld().spawnEntity(location.add(0.0, 1.0, 0.0), EntityType.PRIMED_TNT);
-				((CraftCreature) sheep.getBukkitEntity()).setPassenger(primedTnt);
-				
-				try {
-					Field sourceField = EntityTNTPrimed.class.getDeclaredField("source");
-					sourceField.setAccessible(true);
-					sourceField.set(((CraftTNTPrimed) primedTnt).getHandle(), ((CraftLivingEntity) owner).getHandle());
-				} catch (Exception ex) {
-					ex.printStackTrace();
-				}
-				
-				//((Sheep) sheep.getBukkitEntity()).setColor(color);
+		((World) location.getWorld()).addEntity(sheep, SpawnReason.CUSTOM);
+		TNTPrimed primedTnt = (TNTPrimed) location.getWorld().spawnEntity(location.add(0.0, 1.0, 0.0), EntityType.PRIMED_TNT);
+		((CraftCreature) sheep.getBukkitEntity()).setPassenger(primedTnt);
+		
+		try {
+			Field sourceField = EntityTNTPrimed.class.getDeclaredField("source");
+			sourceField.setAccessible(true);
+			sourceField.set(((CraftTNTPrimed) primedTnt).getHandle(), ((CraftLivingEntity) owner).getHandle());
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		//((Sheep) sheep.getBukkitEntity()).setColor(color);
 
-				sheep.setTNT(primedTnt);
-				sheep.setPosition(location.getX(), location.getY(), location.getZ());
-			}
-		}.runTask(Main.getInstance());
+		sheep.setTNT(primedTnt);
+		sheep.setPosition(location.getX(), location.getY(), location.getZ());
 		
 		return sheep;
 	}
