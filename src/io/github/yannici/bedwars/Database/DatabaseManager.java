cbef75c53e37be6677c855e04ee6a39f8af35120
Yannici <yannicschne@gmail.com>
2015-07-16 12:39:55 +0200

    [1.2.3] Destroy pooled datasource on cleanup

--- src/io/github/yannici/bedwars/Database/DatabaseManager.java
+++ src/io/github/yannici/bedwars/Database/DatabaseManager.java
@@ -11,6 +11,7 @@ import io.github.yannici.bedwars.ChatWriter;
 import io.github.yannici.bedwars.Main;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
+import com.mchange.v2.c3p0.DataSources;
 
 public class DatabaseManager {
 
@@ -56,16 +57,17 @@ public class DatabaseManager {
 			this.dataSource = new ComboPooledDataSource();
 			
 			// currently only mysql is supported
-			dataSource.setDriverClass("com.mysql.jdbc.Driver");
-			dataSource.setJdbcUrl("jdbc:mysql://" + this.host + ":"
+			this.dataSource.setDriverClass("com.mysql.jdbc.Driver");
+			this.dataSource.setJdbcUrl("jdbc:mysql://" + this.host + ":"
 					+ String.valueOf(this.port) + "/" + this.database);
 			
-			dataSource.setUser(this.user);
-			dataSource.setPassword(this.password);
+			this.dataSource.setUser(this.user);
+			this.dataSource.setPassword(this.password);
 			
 			// connection pool configuration
-			dataSource.setMinPoolSize(minPoolSize);
-			dataSource.setMaxPoolSize(maxPoolSize);
+			this.dataSource.setMaxIdleTime(600);
+			this.dataSource.setMinPoolSize(minPoolSize);
+			this.dataSource.setMaxPoolSize(maxPoolSize);
 		} catch(Exception ex) {
 			Main.getInstance().getServer().getConsoleSender().sendMessage(ChatWriter.pluginMessage(ChatColor.RED + "Couldn't create pooled datasource: " + ex.getMessage()));
 		}
@@ -87,7 +89,13 @@ public class DatabaseManager {
 	
 	public void cleanUp() {
 		if(this.dataSource != null) {
-			this.dataSource.close();
+			try {
+				this.dataSource.setMinPoolSize(0);
+				this.dataSource.setInitialPoolSize(0);
+				DataSources.destroy(this.dataSource);
+			} catch (SQLException e) {
+				// just shutdown
+			}
 		}
 	}
 
