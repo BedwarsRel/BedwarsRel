352a102e17a8ee36f3d22e0b4aef96d267c4100c
Yannici <yannicschne@gmail.com>
2015-09-11 17:24:25 +0200

    [1.2.7] Reduced stats delay again on server join

--- src/io/github/yannici/bedwars/Listener/PlayerListener.java
+++ src/io/github/yannici/bedwars/Listener/PlayerListener.java
@@ -115,7 +115,7 @@ public class PlayerListener extends BaseListener {
 		
 		if(Main.getInstance().isHologramsEnabled() 
                 && Main.getInstance().getHolographicInteractor() != null) {
-		    Main.getInstance().getHolographicInteractor().updateHolograms(je.getPlayer(), 60L*5);
+		    Main.getInstance().getHolographicInteractor().updateHolograms(je.getPlayer(), 60L);
 		}
 	}
 	
