17be1751678e2ea07e74e043e04f625a7cf05b91
Block-Build <Block-Build@t-online.de>
2016-03-02 16:22:46 +0100

    Default Block don't become ProtectionWall Issue - #445

--- src/io/github/yannici/bedwars/Shop/Specials/ProtectionWallListener.java
+++ src/io/github/yannici/bedwars/Shop/Specials/ProtectionWallListener.java
@@ -33,6 +33,11 @@ public class ProtectionWallListener implements Listener {
 			return;
 		}
 		
+		if(interact.getItem().getItemMeta().getDisplayName() == null) {
+			return;
+			
+		}
+		
 		Game game = Main.getInstance().getGameManager().getGameOfPlayer(interact.getPlayer());
 		if(game == null) {
 			return;
@@ -58,6 +63,10 @@ public class ProtectionWallListener implements Listener {
 	    ProtectionWall wall = new ProtectionWall();
 	    if(place.getBlock().getType() != wall.getItemMaterial()) {
 	        return;
+	    }
+	    
+	    if(place.getItemInHand().getItemMeta().getDisplayName() == null){
+	    	return;
 	    }
 	    
 	    Game game = Main.getInstance().getGameManager().getGameOfPlayer(place.getPlayer());
