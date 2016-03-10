742a13fbf221f69b1eeed54695e0783e18443b96
Yannici <yannicschne@gmail.com>
2016-03-10 12:06:06 +0100

    possible fix of tntsheep in 1.9

--- src/io/github/yannici/bedwars/Com/v1_9_R1/TNTSheep.java
+++ src/io/github/yannici/bedwars/Com/v1_9_R1/TNTSheep.java
@@ -1,7 +1,7 @@
 package io.github.yannici.bedwars.Com.v1_9_R1;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
+import java.util.Set;
 
 import org.bukkit.Location;
 import org.bukkit.World;
@@ -44,17 +44,20 @@ public class TNTSheep extends EntitySheep implements ITNTSheep {
 		try {
 			Field b = this.goalSelector.getClass().getDeclaredField("b");
 			b.setAccessible(true);
-			b.set(this.goalSelector, new ArrayList<>());
+			
+			Set<?> goals = (Set<?>) b.get(this.goalSelector);
+			goals.clear(); // Clears the goals
+			
 			this.getAttributeInstance(GenericAttributes.FOLLOW_RANGE).setValue(128D);
 			this.getAttributeInstance(GenericAttributes.MOVEMENT_SPEED)
 					.setValue(Main.getInstance().getConfig().getDouble("specials.tntsheep.speed", 0.4D));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
-		this.goalSelector.a(0, new PathfinderGoalBedwarsPlayer(this, 1D, false));
+		
+		this.goalSelector.a(0, new PathfinderGoalBedwarsPlayer(this, 1D, false)); // Add bedwars player goal
 		this.setGoalTarget((EntityLiving) (((CraftPlayer) target).getHandle()),
-				EntityTargetEvent.TargetReason.OWNER_ATTACKED_TARGET, false);
+				EntityTargetEvent.TargetReason.TARGET_ATTACKED_ENTITY, false);
 		((Creature) this.getBukkitEntity()).setTarget((LivingEntity) target);
 	}
 
