401b9320b360260265997ba9be16639d0c76e2cb
Sebastian Binder <sebastianbinder@users.noreply.github.com>
2016-05-10 08:09:31 +0200

    [1.3.1] New shop.yml Layout
    * [1.3.1] WIP new shop.yml
    
    * [1.3.1] Translate and Update shop.yml
    
    resolves #477
    
    * [1.3.1] Add potion updater (no splash potions yet!)
    
    * [1.3.1] fix durations
    
    * [1.3.1] fix typo in default shop.yml
    
    #508
    
    * [1.3.1] Add Potion Name on Updater
    
    * [1.3.1] Add Splash Potion Meta
    
    * [1.3.1] Swap Price and Reward

--- src/main/java/io/github/yannici/bedwars/Main.java
+++ src/main/java/io/github/yannici/bedwars/Main.java
@@ -126,19 +126,20 @@ public class Main extends JavaPlugin {
 
 		this.getConfig().options().copyDefaults(true);
 		this.getConfig().options().copyHeader(true);
+		
+		this.craftbukkit = this.getCraftBukkit();
+		this.minecraft = this.getMinecraftPackage();
+		this.version = this.loadVersion();
 
 		ConfigUpdater configUpdater = new ConfigUpdater();
 		configUpdater.addConfigs();
 		this.saveConfiguration();
 		this.loadConfigInUTF();
+		configUpdater.updateShop();
 		this.loadShop();
 
 		this.isSpigot = this.getIsSpigot();
 		this.loadDatabase();
-
-		this.craftbukkit = this.getCraftBukkit();
-		this.minecraft = this.getMinecraftPackage();
-		this.version = this.loadVersion();
 
 		this.registerCommands();
 		this.registerListener();
