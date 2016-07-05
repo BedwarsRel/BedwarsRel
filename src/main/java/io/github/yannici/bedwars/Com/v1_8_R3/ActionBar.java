d0875220a7ca2fab2dd89f22dfc3cdb9de5eab25
Sebastian Binder <sebastianbinder@users.noreply.github.com>
2016-07-05 08:07:08 +0200

    [1.3.3] Possibility to show teamname in ActionBar (#586)
    resolves #557

---
+++ src/main/java/io/github/yannici/bedwars/Com/v1_8_R3/ActionBar.java
@@ -0,0 +1,20 @@
+package io.github.yannici.bedwars.Com.v1_8_R3;
+
+import org.bukkit.ChatColor;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+import net.minecraft.server.v1_8_R3.IChatBaseComponent;
+import net.minecraft.server.v1_8_R3.PacketPlayOutChat;
+import net.minecraft.server.v1_8_R3.IChatBaseComponent.ChatSerializer;
+
+public class ActionBar {
+
+  public static void sendActionBar(Player player, String message) {
+    String s = ChatColor.translateAlternateColorCodes('&', message.replace("_", " "));
+    IChatBaseComponent icbc = ChatSerializer.a("{\"text\": \"" + s + "\"}");
+    PacketPlayOutChat bar = new PacketPlayOutChat(icbc, (byte) 2);
+    ((CraftPlayer) player).getHandle().playerConnection.sendPacket(bar);
+  }
+
+}
