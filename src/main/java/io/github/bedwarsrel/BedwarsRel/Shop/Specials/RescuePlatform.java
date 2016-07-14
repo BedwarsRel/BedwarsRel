ab89a812d312180c8a2fb2d5f45981691150a4a2
Block-Build <Block-Build@t-online.de>
2016-07-14 17:52:23 +0200

    Fix Platform delay

--- src/main/java/io/github/bedwarsrel/BedwarsRel/Shop/Specials/RescuePlatform.java
+++ src/main/java/io/github/bedwarsrel/BedwarsRel/Shop/Specials/RescuePlatform.java
@@ -3,20 +3,28 @@ package io.github.bedwarsrel.BedwarsRel.Shop.Specials;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 
+import com.google.common.collect.ImmutableMap;
+
+import io.github.bedwarsrel.BedwarsRel.ChatWriter;
 import io.github.bedwarsrel.BedwarsRel.Main;
 import io.github.bedwarsrel.BedwarsRel.Utils;
 import io.github.bedwarsrel.BedwarsRel.Game.Game;
+import io.github.bedwarsrel.BedwarsRel.Game.Team;
 
 public class RescuePlatform extends SpecialItem {
 
   private int livingTime = 0;
-  private Player activatedPlayer = null;
+  private Player owner = null;
   private List<Block> platformBlocks = null;
   private BukkitTask task = null;
   private Game game = null;
@@ -25,6 +33,8 @@ public class RescuePlatform extends SpecialItem {
     super();
 
     this.platformBlocks = new ArrayList<Block>();
+    this.game = null;
+    this.owner = null;
   }
 
   @Override
@@ -36,29 +46,93 @@ public class RescuePlatform extends SpecialItem {
     return this.livingTime;
   }
 
-  public Player getPlayer() {
-    return this.activatedPlayer;
+  public Game getGame() {
+    return this.game;
+  }
+
+  public Player getOwner() {
+    return this.owner;
   }
 
   public void addPlatformBlock(Block block) {
     this.platformBlocks.add(block);
   }
+  
+  @SuppressWarnings("deprecation")
+public void create(Player player, Game game) {
+    this.game = game;
+    this.owner = player;
 
-  public void runTask() {
-    if (Main.getInstance().getConfig().getInt("specials.rescue-platform.break-time", 10) == 0
-        && Main.getInstance().getConfig().getInt("specials.rescue-platform.using-wait-time",
-            20) == 0) {
-      // no break and no wait time ;)
-      return;
+    int breakTime = Main.getInstance().getIntConfig("specials.rescue-platform.break-time", 10);
+    int waitTime = Main.getInstance().getIntConfig("specials.rescue-platform.using-wait-time", 20);
+    boolean canBreak = Main.getInstance().getBooleanConfig("specials.rescue-platform.can-break", false);
+    Material configMaterial = Utils.getMaterialByConfig("specials.rescue-platform.block", Material.STAINED_GLASS);
+  
+    if (waitTime > 0){
+      ArrayList<RescuePlatform> livingPlatforms = this.getLivingPlatforms();
+      if(!livingPlatforms.isEmpty()){
+	    for (RescuePlatform livingPlatform : livingPlatforms){
+          int waitLeft = waitTime - livingPlatform.getLivingTime();
+          if (waitLeft > 0) {
+            player.sendMessage(ChatWriter.pluginMessage(Main._l("ingame.specials.rescue-platform.left",
+                ImmutableMap.of("time", String.valueOf(waitLeft)))));
+	        return; 
+          }
+        }
+      }
     }
 
+	    if (player.getLocation().getBlock().getRelative(BlockFace.DOWN).getType() != Material.AIR) {
+	      player.sendMessage(ChatWriter.pluginMessage(ChatColor.RED + Main._l("errors.notinair")));
+	      return;
+	    }
+
+	    Location mid = player.getLocation().clone();
+	    mid.setY(mid.getY() - 1.0D);
+
+	    Team team = game.getPlayerTeam(player);
+	    ItemStack usedStack = player.getInventory().getItemInHand();
+	    usedStack.setAmount(usedStack.getAmount() - 1);
+	    player.getInventory().setItem(player.getInventory().getHeldItemSlot(), usedStack);
+	    player.updateInventory();
+	    for (BlockFace face : BlockFace.values()) {
+	      if (face.equals(BlockFace.DOWN) || face.equals(BlockFace.UP)) {
+	        continue;
+	      }
+
+	      Block placed = mid.getBlock().getRelative(face);
+	      if (placed.getType() != Material.AIR) {
+	        continue;
+	      }
+
+	      placed.setType(configMaterial);
+	      if (configMaterial.equals(Material.STAINED_GLASS) || configMaterial.equals(Material.WOOL)
+	          || configMaterial.equals(Material.STAINED_CLAY)) {
+	        placed.setData(team.getColor().getDyeColor().getData());
+	      }
+
+	      if (!canBreak) {
+	        game.getRegion().addPlacedUnbreakableBlock(placed, null);
+	      } else {
+	        game.getRegion().addPlacedBlock(placed, null);
+	      }
+
+	      this.addPlatformBlock(placed);
+	    }
+	    if (breakTime > 0 || waitTime > 0) {
+	        this.runTask(breakTime, waitTime);
+	        game.addSpecialItem(this);
+	    }
+  }
+
+  public void runTask(final int breakTime, final int waitTime) {
     this.task = new BukkitRunnable() {
 
       @Override
       public void run() {
         RescuePlatform.this.livingTime++;
-        if (RescuePlatform.this.livingTime == Main.getInstance().getConfig()
-            .getInt("specials.rescue-platform.break-time", 10)) {
+        
+        if (breakTime > 0 && RescuePlatform.this.livingTime == breakTime) {
           for (Block block : RescuePlatform.this.platformBlocks) {
             block.getChunk().load(true);
             block.setType(Material.AIR);
@@ -66,18 +140,7 @@ public class RescuePlatform extends SpecialItem {
           }
         }
 
-        int wait =
-            Main.getInstance().getConfig().getInt("specials.rescue-platform.using-wait-time", 20);
-        if (RescuePlatform.this.livingTime >= wait && wait > 0) {
-          RescuePlatform.this.game.removeRunningTask(this);
-          RescuePlatform.this.game.removeSpecialItem(RescuePlatform.this);
-          RescuePlatform.this.task = null;
-          this.cancel();
-          return;
-        }
-
-        if (wait <= 0 && RescuePlatform.this.livingTime >= Main.getInstance().getConfig()
-            .getInt("specials.rescue-platform.break-time", 10)) {
+        if (RescuePlatform.this.livingTime >= waitTime && RescuePlatform.this.livingTime >= breakTime) {
           RescuePlatform.this.game.removeRunningTask(this);
           RescuePlatform.this.game.removeSpecialItem(RescuePlatform.this);
           RescuePlatform.this.task = null;
@@ -88,19 +151,24 @@ public class RescuePlatform extends SpecialItem {
     }.runTaskTimer(Main.getInstance(), 20L, 20L);
     this.game.addRunningTask(this.task);
   }
+  
+  private ArrayList<RescuePlatform> getLivingPlatforms() {
+    ArrayList<RescuePlatform> livingPlatforms = new ArrayList<RescuePlatform>();
+    for (SpecialItem item : game.getSpecialItems()) {
+      if (item instanceof RescuePlatform) {
+        RescuePlatform rescuePlatform = (RescuePlatform) item;
+        if (rescuePlatform.getOwner().equals(this.getOwner())) {
+          livingPlatforms.add(rescuePlatform);
+	    }
+      }
+	}
+    return livingPlatforms;
+  }
 
   @Override
   public Material getActivatedMaterial() {
     // not needed
     return null;
-  }
-
-  public void setActivatedPlayer(Player player) {
-    this.activatedPlayer = player;
-  }
-
-  public void setGame(Game game) {
-    this.game = game;
   }
 
 }
