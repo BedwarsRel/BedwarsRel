56e5eb6ae14529b9cde5a59b9e5a021fb3d48565
Sebastian Binder <designstudio@sebastianbinder.com>
2016-09-02 08:25:47 +0200

    Support Data Command

--- src/main/java/io/github/bedwarsrel/BedwarsRel/Utils/SupportData.java
+++ src/main/java/io/github/bedwarsrel/BedwarsRel/Utils/SupportData.java
@@ -1,9 +1,16 @@
 package io.github.bedwarsrel.BedwarsRel.Utils;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.util.ArrayList;
 import java.util.Collections;
 
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
 
 import io.github.bedwarsrel.BedwarsRel.Main;
@@ -18,7 +25,8 @@ public class SupportData {
   private String bukkitVersion;
   private String serverMode;
   private String identifier;
-  private String plugins;
+  private ArrayList<String> plugins;
+  private ArrayList<String> javaInformation;
 
   public SupportData() {
     this.pluginVersion = Main.getInstance().getDescription().getVersion();
@@ -43,19 +51,56 @@ public class SupportData {
       e.printStackTrace();
     }
 
-    this.plugins = "";
+    this.plugins = new ArrayList<String>();
 
     Plugin[] plugins = Main.getInstance().getServer().getPluginManager().getPlugins();
-    int pluginsAmount = plugins.length;
-    int pluginsCount = 0;
     for (Plugin plugin : plugins) {
-      this.plugins =
-          this.plugins + plugin.getName() + " (" + plugin.getDescription().getVersion() + ")";
-      if (pluginsCount < pluginsAmount - 1) {
-        this.plugins = this.plugins + ", ";
-      }
-      pluginsCount++;
+      this.plugins.add(plugin.getName() + " (" + plugin.getDescription().getVersion() + ")");
     }
 
+    this.javaInformation = new ArrayList<String>();
+    Runtime runtime = Runtime.getRuntime();
+
+    this.javaInformation.add("memory.free: " + runtime.freeMemory());
+    this.javaInformation.add("memory.max: " + runtime.maxMemory());
+    this.javaInformation
+        .add("java.specification.version: " + System.getProperty("java.specification.version"));
+    this.javaInformation.add("java.vendor: " + System.getProperty("java.vendor"));
+    this.javaInformation.add("java.version: " + System.getProperty("java.version"));
+    this.javaInformation.add("os.arch: " + System.getProperty("os.arch"));
+    this.javaInformation.add("os.name: " + System.getProperty("os.name"));
+    this.javaInformation.add("os.version:  " + System.getProperty("os.version"));
+    this.javaInformation.add("os.name: " + System.getProperty("os.name"));
+    this.javaInformation.add("os.name: " + System.getProperty("os.name"));
+  }
+
+  public File getConfigFile() {
+    File configFile = new File(Main.getInstance().getDataFolder(), "config.yml");
+    File tmp = null;
+
+    YamlConfiguration config = new YamlConfiguration();
+    try {
+      BufferedReader reader =
+          new BufferedReader(new InputStreamReader(new FileInputStream(configFile), "UTF-8"));
+      config.load(reader);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    config.set("database.user", "***");
+    config.set("database.password", "***");
+
+    try {
+      tmp = File.createTempFile("bedwarsrel-supportdata-config", null);
+      config.save(tmp);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    
+    return tmp;
+  }
+
+  public File getShopConfigFile() {
+    return new File(Main.getInstance().getDataFolder(), "shop.yml");
   }
 }
